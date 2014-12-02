fa0e2bdc0f0949f2100ffed4f31e882e562534ef
Windsdon <Windsdon@users.noreply.github.com>
2014-12-01 22:09:20 -0200

    not working

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void vec_times(const double *v, double x, double *va, int size){
 	int i;
@@ -66,24 +67,83 @@ double expected(double x){
 }
 
 void line(double x, double vc, double ve) {
-	printf("%20.15f\t%20.15f\t%20.15f\t%20g\n", x, vc, ve, fabs(ve - vc));
+	printf("%13.10f\t%13.10f\t%13.10f\t%13.5e\n", x, vc, ve, fabs(ve - vc));
 }
 
+/*
+*  Cria um vetor com os valores para montar a tabela de erros
+*/
+void make_vals(double x, double ym, double ye, double *vec) {
+	vec = (double*) malloc(4 * sizeof(double));
+	vec[0] = x;
+	vec[1] = ym;
+	vec[2] = ye;
+	vec[3] = fabs(ye - ym);
+}
+
+typedef struct {
+	int count;
+	double h;
+	double **points;
+} erros;
+
 int main() {
-	int i, j;
+	int i, j, iters;
+	erros **todosErros;
+
+	todosErros = (erros*) malloc(4*sizeof(erros*));
 
 
-	for(j = 0; j < 10; j++){
+	for(j = 0; j < 4; j++){
 		double y[] = {1, 0};
 		double x = 0;
 		double h = 0.1/pow(2, j);
+		double ye;
+		erros *err = (erros*) malloc(sizeof(erros));
+		todosErros[j] = err;
 
+		printf("\n****\nh=%13f\n%13c\t%13c\t%13s\t%13c\n", h, 'x', 'y', "yc", 'e');
 
-		printf("h=%f\n%20c\t%20c\t%20s\t%20c\n", h, 'x', 'y', "yc", 'e');
+		ye = expected(x);
+		line(x, y[0], ye);
 
-		x = euler_mod(y, _f, x, h, (int) ceil(1.0/h), 2);
+		iters = (int) ceil(1.0/h);
+		err->count = iters + 1;
+		err->points = (double**) malloc((iters + 1) * sizeof(double*));
+		err->h = h;
 
-		line(x, y[0], expected(x));
+		make_vals(x, y[0], ye, err->points[0]);
+
+		for(i = 0; i < iters; i++){
+			x = euler_mod(y, _f, x, h, 1, 2);
+			ye = expected(x);
+
+			line(x, y[0], ye);			
+			make_vals(x, y[0], ye, err->points[i + 1]);
+		}
+
+		printf("erros: h=%g, count=%d, points=%p\n", err->h, err->count, err->points);
+	}
+
+	printf("\n\nh\tE\tn\n");
+	for(i = 0; i < 4; i++){
+		erros *err = todosErros[i];
+		double e, dif = 0, sum = 0;
+
+		printf("start sum");
+
+		for(j = 0; j < err->count; j++){
+			printf("%g", err->points[j][2]);
+			dif += pow(err->points[j][1] - err->points[j][2], 2);
+			sum += pow(err->points[j][2], 2);
+			printf("\n j = %d, dif = %g, sum = %g", j, dif, sum);
+		}
+		printf("end sum");
+
+		e = sqrt(dif/sum);
+
+		printf("%g %g %d\n", err->h, e, err->count - 1);
+
 	}
 
 	return 0;
